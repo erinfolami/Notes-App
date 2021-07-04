83eef37da2c2ee3bebfac1c6c58ff44745871e67
erinfolami <emmanuelerinfolami2@gmail.com>
2021-07-04 15:13:27 +0100

    added title and notes in Add Notes activity

--- app/src/main/java/com/example/notes/addNoteActivity.java
+++ app/src/main/java/com/example/notes/addNoteActivity.java
@@ -3,12 +3,28 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class addNoteActivity extends AppCompatActivity {
-
+    private ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateBack();
+            }
+        });
+    }
+
+    public void navigateBack() {
+        finish();
     }
 }
