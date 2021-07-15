448cc872f778aec9fed15ebf9b90558ea1a97914
erinfolami <emmanuelerinfolami2@gmail.com>
2021-07-15 12:30:58 +0100

    moved the ItemTouchHelper method to a seperate class

--- app/src/main/java/com/example/notes/activities/MainActivity.java
+++ app/src/main/java/com/example/notes/activities/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.ImageButton;
 import com.example.notes.R;
 import com.example.notes.models.Model;
 import com.example.notes.recyclerview.RecyclerViewAdapter;
+import com.example.notes.recyclerview.RecyclerViewItemTouchHelper;
 import com.example.notes.sqlite.DatabaseHandler;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected static RecyclerViewAdapter recyclerViewAdapter;
     protected static List<Model> dataArrayList;
 
-    protected Model deletedNote;
+//    protected Model deletedNote;
 
 
     @Override
@@ -41,7 +42,14 @@ public class MainActivity extends AppCompatActivity {
 
         showNoteOnRecyclerView(this);
 
-        deleteNoteOnSwipe();
+
+        //passing the RecyclerViewItemTouchHelper callback to the ItemTouchHelper constrctor,
+        // to receives events when user performs these actions in the callback
+        ItemTouchHelper recyclerViewItemTouchHelper =
+                new ItemTouchHelper(new RecyclerViewItemTouchHelper(MainActivity.this, dataArrayList, recyclerViewAdapter, recyclerView));
+        // attaches the touch helper to recycler view
+        recyclerViewItemTouchHelper.attachToRecyclerView(recyclerView);
+
 
         addNoteButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,75 +77,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerViewAdapter = new RecyclerViewAdapter(dataArrayList, context);
 
         recyclerView.setAdapter(recyclerViewAdapter);
-    }
-
-    public void deleteNoteOnSwipe() {
-        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
-            @Override
-            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-                //position of the ListItem that was swiped
-                int position = viewHolder.getAbsoluteAdapterPosition();
-
-
-                //storing the note that will be deleted in a "deletedNote" variable before deletion
-                //in case a user Un-do's the delete
-                deletedNote = dataArrayList.get(position);
-
-                //Sets the swiped ListItem position to the ID variable in Model class
-                Model model = new Model();
-                model.setId(dataArrayList.get(position).getId());
-
-                //removes the item from the List
-                dataArrayList.remove(position);
-                //notifies the recyclerViewAdapter an item has been removed
-                recyclerViewAdapter.notifyItemRemoved(position);
-
-                //Creating a Snackbar to give an option to undo delete
-                int duration = Snackbar.LENGTH_LONG;
-                String message = "Note deleted";
-                Snackbar snackbar = Snackbar.make(recyclerView, message, duration);
-                snackbar.setAction("Undo", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //adds the item back to the dataArrayList when Undo is clicked
-                        dataArrayList.add(position, deletedNote);
-                        //notifies the the recyclerView an item has been inserted in the dataArrayList
-                        // and inserts the data back to its position
-                        recyclerViewAdapter.notifyItemInserted(position);
-                    }
-                });
-                snackbar.addCallback(new Snackbar.Callback() {
-                    @Override
-                    public void onShown(Snackbar sb) {
-                        super.onShown(sb);
-                    }
-
-                    @Override
-                    public void onDismissed(Snackbar transientBottomBar, int event) {
-                        super.onDismissed(transientBottomBar, event);
-                        //checks if stackbar was not dismissed via an action click (Undo)
-                        if (event != BaseTransientBottomBar.BaseCallback.DISMISS_EVENT_ACTION) {
-
-//                          //deletes the swiped note from database by using the ID
-                            DatabaseHandler databaseHandler = new DatabaseHandler(MainActivity.this);
-                            databaseHandler.deleteNote(model);
-                        }
-                    }
-                });
-                snackbar.show();
-
-            }
-        };
-
-
-        // attaching the touch helper to recycler view
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
-        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 
 
---
+++ app/src/main/java/com/example/notes/recyclerview/RecyclerViewItemTouchHelper.java
@@ -0,0 +1,105 @@
+package com.example.notes.recyclerview;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.notes.activities.MainActivity;
+import com.example.notes.models.Model;
+import com.example.notes.sqlite.DatabaseHandler;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+//This utility class  adds swipe to dismiss(delete,archive etc) and drag & drop support to RecyclerView.
+//This is a Callback class, which configures what type of interactions are enabled,
+
+public class RecyclerViewItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+
+    protected List<Model> dataArrayList;
+    protected RecyclerViewAdapter recyclerViewAdapter;
+    protected RecyclerView recyclerView;
+    protected Context context;
+
+
+    public RecyclerViewItemTouchHelper(Context context, List<Model> dataArrayList, RecyclerViewAdapter recyclerViewAdapter, RecyclerView recyclerView) {
+        super(0, ItemTouchHelper.LEFT);
+        this.dataArrayList = dataArrayList;
+        this.recyclerViewAdapter = recyclerViewAdapter;
+        this.recyclerView = recyclerView;
+        this.context = context;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        // removes notes from RecyclerView
+
+
+        Model deletedNote;
+
+        //position of the ListItem that was swiped
+        int position = viewHolder.getAbsoluteAdapterPosition();
+
+        //storing the note that will be deleted in a "deletedNote" variable before deletion
+        //in case a user Un-do's the delete
+        deletedNote = dataArrayList.get(position);
+
+        //Sets the swiped ListItem position to the ID variable in Model class
+        Model model = new Model();
+        model.setId(dataArrayList.get(position).getId());
+
+        //removes the item from the List
+        dataArrayList.remove(position);
+        //notifies the recyclerViewAdapter an item has been removed
+        recyclerViewAdapter.notifyItemRemoved(position);
+
+        //Creating a Snackbar to give an option to undo delete
+        int duration = Snackbar.LENGTH_LONG;
+        String message = "Note deleted";
+        Snackbar snackbar = Snackbar.make(recyclerView, message, duration);
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //adds the item back to the dataArrayList when Undo is clicked
+                dataArrayList.add(position, deletedNote);
+                //notifies the the recyclerView an item has been inserted in the dataArrayList
+                // and inserts the data back to its position
+                recyclerViewAdapter.notifyItemInserted(position);
+            }
+        });
+        snackbar.addCallback(new Snackbar.Callback() {
+            @Override
+            public void onShown(Snackbar sb) {
+                super.onShown(sb);
+            }
+
+            @Override
+            public void onDismissed(Snackbar transientBottomBar, int event) {
+                super.onDismissed(transientBottomBar, event);
+                //checks if stackbar was not dismissed via an action click (Undo)
+                if (event != BaseTransientBottomBar.BaseCallback.DISMISS_EVENT_ACTION) {
+
+//                          //deletes the swiped note from database by using the ID
+                    DatabaseHandler databaseHandler = new DatabaseHandler(context);
+                    databaseHandler.deleteNote(model);
+                }
+            }
+        });
+        snackbar.show();
+
+
+    }
+
+}
+
+
+
